c8d6cb3f22262a82ddb6b73ad7859ad733a4d1a9
shevadon <shevadon01@gmail.com>
2020-12-09 14:55:06 +0200

    configured global styles for application + 404

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,21 @@
 import React from "react";
-import '../utils/global-styles.css';
+import styled from '../../node_modules/styled-components';
 
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../utils/global-styles.css';
+import {layoutStyles} from "../utils/styled-layout";
+
+
+
+const LayoutWrapper = styled('div')`
+    ${layoutStyles};
+`;
 
 const Main = () => {
     return(
-        <>
+        <LayoutWrapper>
             <h1>Main Page</h1>
-        </>
+        </LayoutWrapper>
     );
 }
 

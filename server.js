0529f1fda5e6ba745b97a7eb2e513b268ca4c6e7
andrey.shevchenko <shevadon01@gmail.com>
2021-01-11 09:25:42 +0200

    deploy server.js

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const router = express.Router();
 
 //create app
 const app = express();
+let port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server is running on 5000 PORT"));
+app.listen(port, () => console.log("Server is running on 5000 PORT"));
 
 
 //Set General Email Options
